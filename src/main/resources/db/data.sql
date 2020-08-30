96261f6f5dd3eadbb9a01851aa136d3e8bdd4742
user01 <eulerlcs@163.com>
2020-08-30 19:08:31 +0900

    title修正

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO `hello`.`user` (`name`, `department`, `created_at`) VALUES ('zhangsan', '開発一部', '2020-08-30 02:09:53');
-INSERT INTO `hello`.`user` (`name`, `department`, `created_at`) VALUES ('lisi', '開発二部', '2020-08-30 02:09:53');
-INSERT INTO `hello`.`user` (`name`, `department`, `created_at`) VALUES ('liuchunsheng', '開発二部', '	2020-08-30 02:09:53');
+INSERT INTO `hello`.`user` (`name`, `department`, `created_at`) VALUES ('zhangsan', 'department 1', '2020-08-30 02:09:53');
+INSERT INTO `hello`.`user` (`name`, `department`, `created_at`) VALUES ('lisi', 'department 2', '2020-08-30 02:09:53');
+INSERT INTO `hello`.`user` (`name`, `department`, `created_at`) VALUES ('liuchunsheng', 'department 2', '2020-08-30 02:09:53');
