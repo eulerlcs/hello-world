1da15c87045bf37176b8595dc49afad3ea4b69d5
member-01 <liu-chunsheng@zhimingsoft.jp>
2020-09-03 09:55:01 +0900

    v1.0.8

--- src/test/java/com/github/eulerlcs/hello/service/UserServiceTest.java
+++ src/test/java/com/github/eulerlcs/hello/service/UserServiceTest.java
@@ -1,8 +1,6 @@
 package com.github.eulerlcs.hello.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,24 +16,9 @@ class UserServiceTest {
 
 	@Test
 	void testSelectUser() {
-		ss1();
 		User user = userService.selectUser(1);
 		assertEquals(1, user.getId());
 		assertEquals("zhangsan", user.getName());
-		assertEquals("d2", user.getDepartment());
-	}
-
-	private void ss1() {
-		User mockUser = new User();
-		mockUser.setId(1);
-		mockUser.setName("zhangsan");
-		mockUser.setDepartment("d2");
-
-		UserMapper userMapper = mock(UserMapper.class);
-
-		when(userMapper.selectById(1)).thenReturn(mockUser);
-
-		// the following prints "first"
-		System.out.println(userMapper.selectById(1));
+		assertEquals("department 1", user.getDepartment());
 	}
 }
