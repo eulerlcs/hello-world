ff7014b7c64505b5d601caf596e601096e58216e
leader-01 <eulerlcs@yahoo.co.jp>
2020-09-03 09:38:56 +0900

    Dev/ryu

--- src/test/java/com/github/eulerlcs/hello/service/UserServiceTest.java
+++ src/test/java/com/github/eulerlcs/hello/service/UserServiceTest.java
@@ -1,12 +1,15 @@
 package com.github.eulerlcs.hello.service;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.github.eulerlcs.hello.domain.User;
+import com.github.eulerlcs.hello.mapper.UserMapper;
 
 @SpringBootTest
 class UserServiceTest {
@@ -15,8 +18,24 @@ class UserServiceTest {
 
 	@Test
 	void testSelectUser() {
+		ss1();
 		User user = userService.selectUser(1);
-		assertNotNull(user);
+		assertEquals(1, user.getId());
+		assertEquals("zhangsan", user.getName());
+		assertEquals("d2", user.getDepartment());
 	}
 
+	private void ss1() {
+		User mockUser = new User();
+		mockUser.setId(1);
+		mockUser.setName("zhangsan");
+		mockUser.setDepartment("d2");
+
+		UserMapper userMapper = mock(UserMapper.class);
+
+		when(userMapper.selectById(1)).thenReturn(mockUser);
+
+		// the following prints "first"
+		System.out.println(userMapper.selectById(1));
+	}
 }
